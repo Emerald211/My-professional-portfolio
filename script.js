ab1952112eef1c722c7d901f00db1bdc820987d4
Emerald211 <aderibigbeemmanuel2004@gmail.com>
2022-06-08 11:09:15 -1200

    portfolio

---
+++ script.js
@@ -0,0 +1,36 @@
+function goToSpike() {
+    const url = "https://emerald211.github.io/Spikes-Website/"
+    window.open(url, "_blank")
+}
+
+function goToQuote() {
+    const quote = "https://emerald211.github.io/quote-generator/"
+    window.open(quote, "_blank")
+}
+
+function goToJoke() {
+    const joke = "https://emerald211.github.io/JOKE-TELLER/"
+    window.open(joke, "_blank")
+}
+
+function goToScroll() {
+    const scroll = "https://emerald211.github.io/infinite-scroll/"
+    window.open(scroll, "_blank")
+}
+
+function goToLInkedin() {
+    const link = "https://www.linkedin.com/mwlite/in/emmanuel-aderibigbe-1ab984235"
+    window.open(link, "_blank")
+}
+
+function goToInsta() {
+    const insta = "https://www.instagram.com/emmanueladeribigbe01"
+    window.open(insta, "_blank")
+}
+
+function goToGithub() {
+    const github = "https://github.com/Emerald211"
+    window.open(github, "_blank")
+}
+
+
